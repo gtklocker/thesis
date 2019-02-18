0886dcd1204779de48b52b0f938f5c3b3a6c0eb5
Kostis Karantias <kkarantias@gmail.com>
2019-02-18 18:23:48 +0200

    support litecoin pow

--- experiments/plot_mainnet_levels.py
+++ experiments/plot_mainnet_levels.py
@@ -1,18 +1,26 @@
-from hashlib import sha256
+from hashlib import sha256, scrypt
 import math
 import itertools
 
 def doublesha256(x):
     return sha256(sha256(x).digest()).digest()
 
-def hdr_to_id(hdr):
+def bitcoin_hdr_to_id(hdr):
     return doublesha256(hdr)
 
-def hdr_to_human_id(hdr):
-    return hdr_to_id(hdr)[::-1].hex()
+def litecoin_hdr_to_id(hdr):
+    return scrypt(password=hdr, salt=hdr, n=1024, r=1, p=1, dklen=32)
 
 BITCOIN_TARGET =  0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-LITECOIN_TARGET = 0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2
+LITECOIN_TARGET = 0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
+def block_ids_from_file(file_name, hdr_to_id=bitcoin_hdr_to_id):
+    with open(file_name, "rb") as f:
+        for height in itertools.count():
+            hdr = f.read(HEADER_SIZE)
+            if len(hdr) != HEADER_SIZE:
+                break
+            yield hdr_to_id(hdr)
 
 def level(block_id, target=None):
     target = BITCOIN_TARGET if target is None else target
@@ -22,29 +30,26 @@ def level(block_id, target=None):
 
 HEADER_SIZE = 80
 
-def levels_for_file(file_name, target=None):
-    with open(file_name, "rb") as f:
-        for height in itertools.count():
-            hdr = f.read(HEADER_SIZE)
-            if len(hdr) != HEADER_SIZE:
-                break
-            yield level(hdr_to_human_id(hdr), target)
+def levels_for_file(file_name, hdr_to_id=bitcoin_hdr_to_id, target=None):
+    for blkid in block_ids_from_file(file_name, hdr_to_id):
+        yield level(blkid[::-1].hex(), target)
 
 def bitcoin_cash_levels():
     yield from levels_for_file("BitcoinCash-Mainnet.bin")
 def bitcoin_core_levels():
     yield from levels_for_file("BitcoinCore-Mainnet.bin")
 def litecoin_levels():
-    yield from levels_for_file("Litecoin-Mainnet.bin", LITECOIN_TARGET)
+    yield from levels_for_file("Litecoin-Mainnet.bin", litecoin_hdr_to_id, LITECOIN_TARGET)
 
 def column_for_levels(levels):
     return pd.DataFrame(levels).cummax()[0]
 
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.concat(
-        [column_for_levels(lvls)
-            for lvls in [bitcoin_core_levels(), bitcoin_cash_levels(), litecoin_levels()]]
-        , axis=1, keys=["Bitcoin", "Bitcoin Cash", "Litecoin"])
-df.plot().set(ylabel="interlink size", xlabel="block height")
-plt.show()
+if __name__ == "__main__":
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    df = pd.concat(
+            [column_for_levels(lvls)
+                for lvls in [bitcoin_core_levels(), bitcoin_cash_levels()]]#, litecoin_levels()]]
+            , axis=1, keys=["Bitcoin", "Bitcoin Cash"])#, "Litecoin"])
+    df.plot(logy=True).set(ylabel="interlink size", xlabel="block height")
+    plt.show()
