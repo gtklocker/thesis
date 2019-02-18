b40cd7092af985a51311c9a700662ddcbfc44638
Kostis Karantias <kkarantias@gmail.com>
2019-02-18 21:10:34 +0200

    actually do both plots on interlink compression

--- experiments/plot_interlink_compression.py
+++ experiments/plot_interlink_compression.py
@@ -22,18 +22,27 @@ def plot_commitment_comparison(hdr_file):
     df.plot()
     plt.show()
 
-def plot_proof_comparison(hdr_file):
+def df_from_file(hdr_file):
     df = columns_for_sizes(interlink_sizes(hdr_file))
     df['savings'] = 1-df['blockset']/df['blocklist']
     df['blockset_proof'] = np.log2(df['blockset'])
     df['blocklist_proof'] = np.log2(df['blocklist'])
-    df['blockset_proof_savings'] = 1-df['blockset_proof']/df['blocklist_proof']
+    return df
 
-    #df[['blocklist_proof', 'blockset_proof']].rolling(2000).mean().plot() \
-    #    .set(ylabel="# of block ids in interlink inclusion proof", xlabel="block height")
+def plot_proof_savings(hdr_file):
+    df = df_from_file(hdr_file)
+    df['blockset_proof_savings'] = 1-df['blockset_proof']/df['blocklist_proof']
     (df['blockset_proof_savings'] * 100).rolling(2000).mean().plot() \
         .set(ylabel="interlink inclusion proof savings (%)", xlabel="block height")
-    plt.show()
+
+def plot_proof_cmp(hdr_file):
+    df = df_from_file(hdr_file)
+    df[['blocklist_proof', 'blockset_proof']] \
+        .rename(columns={"blocklist_proof": "Block List", "blockset_proof": "Block Set"}) \
+        .rolling(2000).mean().plot() \
+        .set(ylabel="# of block ids in interlink inclusion proof", xlabel="block height")
 
 if __name__ == "__main__":
-    plot_proof_comparison("BitcoinCash-Mainnet.bin")
+    plot_proof_savings("BitcoinCash-Mainnet.bin")
+    plot_proof_cmp("BitcoinCash-Mainnet.bin")
+    plt.show()
