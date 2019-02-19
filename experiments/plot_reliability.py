efc16c5f3689f83010091dc14856c3a6edc90a03
Kostis Karantias <kkarantias@gmail.com>
2019-02-19 21:27:39 +0200

    nicer order in reliability figure

--- experiments/plot_reliability.py
+++ experiments/plot_reliability.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('./reliability.csv')
 df = df[df.height >= 1272000]
 df.loc[df.missingPtrs == -1, 'missingPtrs'] = r'$\infty$'
-counts = df['missingPtrs'].value_counts(normalize=True) * 100
+counts = df['missingPtrs'].value_counts(normalize=True, sort=False) * 100
 counts.plot(kind='bar', rot=False) \
     .set(xlabel='missing pointers', ylabel='% of blocks')
 plt.show()
