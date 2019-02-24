f7c8f90f13776d6f1a092659b7e68df467b1a10b
Kostis Karantias <kkarantias@gmail.com>
2019-02-24 10:58:38 +0200

    update log var plot

--- experiments/plot_log_var.py
+++ experiments/plot_log_var.py
@@ -1,12 +1,16 @@
+from itertools import tee
 from science import *
 
 def log_var_for_file(file_name, hdr_to_id=bitcoin_hdr_to_id, target=None):
     interlink_size = 1
-    for hdr in hdrs_from_file(file_name):
+    gen, blkhdrs = tee(hdrs_from_file(file_name))
+    genesis = next(gen)
+    genesis_target = header_to_target(genesis)
+    for hdr in blkhdrs:
         blkid = hdr_to_id(hdr)
         target = header_to_target(hdr)
         interlink_size = max(interlink_size, level(blkid[::-1].hex()) + 1)
-        yield math.log2(interlink_size + math.log2(float(target)/float(BITCOIN_TARGET)))
+        yield interlink_size + math.log2(float(target)/float(genesis_target))
 
 def bitcoin_cash_log_var():
     yield from log_var_for_file("BitcoinCash-Mainnet.bin")
@@ -25,5 +29,5 @@ if __name__ == "__main__":
                 for lvls in [bitcoin_core_log_var(), bitcoin_cash_log_var()]]
             , axis=1, keys=["Bitcoin", "Bitcoin Cash"]).astype(float)
     print(df)
-    df.plot().set(ylabel="log2(|interlink| + log2(vartarget / genesistarget))", xlabel="block height")
+    df.plot(logx=True).set(ylabel="|interlink| + log2(vartarget / genesistarget)", xlabel="block height")
     plt.show()
