73b8b1d28cf3d0795a33b14e628a827dd2de23cd
Kostis Karantias <kkarantias@gmail.com>
2019-02-24 10:57:00 +0200

    show interlink sizes, enable ltc

--- experiments/plot_mainnet_levels.py
+++ experiments/plot_mainnet_levels.py
@@ -1,14 +1,14 @@
 from science import *
 
 def column_for_levels(levels):
-    return pd.DataFrame(levels)[0]#.cummax()[0]
+    return pd.DataFrame(levels).cummax()[0]
 
 if __name__ == "__main__":
     import pandas as pd
     import matplotlib.pyplot as plt
     df = pd.concat(
             [column_for_levels(lvls)
-                for lvls in [bitcoin_core_levels(), bitcoin_cash_levels()]]#, litecoin_levels()]]
-            , axis=1, keys=["Bitcoin", "Bitcoin Cash"])#, "Litecoin"])
+                for lvls in [bitcoin_cash_levels(), bitcoin_core_levels(), litecoin_levels()]]
+            , axis=1, keys=["Bitcoin Cash", "Bitcoin", "Litecoin"])
     df.plot().set(ylabel="interlink size", xlabel="block height")
     plt.show()
