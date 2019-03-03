d6eb77a61d9444f1f874da3f8f7ffb6fb4a47527
Kostis Karantias <kkarantias@gmail.com>
2019-03-03 17:40:58 +0200

    final superblock density plot

--- experiments/plot_new_levels.py
+++ experiments/plot_new_levels.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 
 from science import *
 
@@ -27,25 +28,19 @@ def windowed_counts(df):
     length = len(df)
 
     max_window_size = window_size(MAX_LEVEL)
-    #print('max_window_size=%d' % max_window_size)
     center_el = max_window_size // 2
-    #print('center_el=%d' % center_el)
 
     assert center_el + max_window_size // 2 + 1 < length
 
     for i in range(min(length, max_window_size)):
         lvl = df[i]
-        #print('actual level %d' % lvl)
         for l in range(lvl+1):
             if l <= MAX_LEVEL:
                 left_el = center_el - window_size(l) // 2
                 right_el = center_el + window_size(l) // 2
-                #print('left_el=%d, i=%d, right_el=%d' % (left_el, i, right_el))
                 if left_el <= i <= right_el:
-                    #print('pass level %d' % l)
                     counts[l] += 1
 
-    #print({k: counts[k]/window_size(k) for k in levels()})
     yield {k: counts[k]/window_size(k) for k in levels()}
     while center_el + max_window_size // 2 + 1 < length:
         for lvl in levels():
@@ -55,14 +50,13 @@ def windowed_counts(df):
                 counts[lvl] -= 1
             if df[new_el] >= lvl:
                 counts[lvl] += 1
-        #print({k: counts[k]/window_size(k) for k in levels()})
         yield {k: counts[k]/window_size(k) for k in levels()}
         center_el += 1
 
-#fake_levels_df = pd.DataFrame([0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4])[0]
-#print(fake_levels_df)
+
 win = pd.DataFrame(windowed_counts(levels_df), dtype='int32').fillna(0)
 win.columns = win.columns.to_series().apply(lambda mu: '%d-superblocks' % mu)
-print(win)
-win.plot(logy=True).set(ylabel="superblock density", xlabel="block height")
+ax = win.plot(logy=True)
+ax.set(ylabel="superblock density", xlabel="block height")
+ax.set_yscale('log', basey=2)
 plt.show()
